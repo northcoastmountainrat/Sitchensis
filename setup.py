cdce9498a5917f598278d61213d776b7a4fa9bd1
northcoastmountainrat <38056445+northcoastmountainrat@users.noreply.github.com>
2018-04-12 19:08:22 -0700

    Cleanup

--- setup.py
+++ setup.py
@@ -20,104 +20,35 @@ with open(path.join(here, 'README.md'), encoding='utf-8') as f:
 # Fields marked as "Optional" may be commented out.
 
 setup(
-    # This is the name of your project. The first time you publish this
-    # package, this name will be registered for you. It will determine how
-    # users can install this project, e.g.:
-    #
-    # $ pip install sampleproject
-    #
-    # And where it will live on PyPI: https://pypi.org/project/sampleproject/
-    #
-    # There are some restrictions on what makes a valid project name
-    # specification here:
-    # https://packaging.python.org/specifications/core-metadata/#name
-    name='sitchensis',  # Required
-
-    # Versions should comply with PEP 440:
-    # https://www.python.org/dev/peps/pep-0440/
-    #
-    # For a discussion on single-sourcing the version across setup.py and the
-    # project code, see
-    # https://packaging.python.org/en/latest/single_source_version.html
-    version='1.0.0',  # Required
-
-    # This is a one-line description or tagline of what your project does. This
-    # corresponds to the "Summary" metadata field:
-    # https://packaging.python.org/specifications/core-metadata/#summary
-    description='Error checking and visualization for tree mapping data',  # Required
-
-    # This is an optional longer description of your project that represents
-    # the body of text which users will see when they visit PyPI.
-    #
-    # Often, this is the same as your README, so you can just read it in from
-    # that file directly (as we have already done above)
-    #
-    # This field corresponds to the "Description" metadata field:
-    # https://packaging.python.org/specifications/core-metadata/#description-optional
-    long_description=long_description,  # Optional
-
-    # Denotes that our long_description is in Markdown; valid values are
-    # text/plain, text/x-rst, and text/markdown
-    #
-    # Optional if long_description is written in reStructuredText (rst) but
-    # required for plain-text or Markdown; if unspecified, "applications should
-    # attempt to render [the long_description] as text/x-rst; charset=UTF-8 and
-    # fall back to text/plain if it is not valid rst" (see link below)
-    #
-    # This field corresponds to the "Description-Content-Type" metadata field:
-    # https://packaging.python.org/specifications/core-metadata/#description-content-type-optional
-    long_description_content_type='text/markdown',  # Optional (see note above)
-
-    # This should be a valid link to your project's main homepage.
-    # This field corresponds to the "Home-Page" metadata field:
-    # https://packaging.python.org/specifications/core-metadata/#home-page-optional
-    url='https://github.com/northcoastmountainrat/Sitchensis',  # Optional
-
+    name='sitchensis',  
+    version='1.0.0',  
+    description='Error checking and visualization for tree mapping data',  
+    long_description=long_description, 
+    long_description_content_type='text/markdown', 
+    url='https://github.com/northcoastmountainrat/Sitchensis', 
     author='Russell Kramer',  # Optional
-
-    # This should be a valid email address corresponding to the author listed
-    # above.
-    author_email='russelld.kramer@gmail.com',  # Optional
-
-    # Classifiers help users find your project by categorizing it.
-    #
-    # For a list of valid classifiers, see
-    # https://pypi.python.org/pypi?%3Aaction=list_classifiers
+    author_email='russelld.kramer@gmail.com',  
     classifiers=[
         'Development Status :: 2 - Pre-Alpha',
-
-        # Indicate who your project is intended for
         'Intended Audience :: CanopyScientists',
         'Topic :: TreeMapping :: Visualization and error screening',
-
-        # Pick your license as you wish
         'License :: OSI Approved :: MIT License',
-
-        # Specify the Python versions you support here. In particular, ensure
-        # that you indicate whether you support Python 2, Python 3 or both.
         'Programming Language :: Python :: 3.x',
         ],
-
-    # This field adds keywords for your project which will appear on the
-    # project page. What does your project relate to?
-    #
-    # Note that this is a string of words separated by whitespace, not a list.
     keywords='error checking, data visualization, 3D', 
-
-    packages=find_packages(exclude=['contrib', 'docs', 'SitchenisTests', 'Templates']),  # Required
-
+    packages=find_packages(exclude=['contrib', 'docs', 'SitchenisTests', 'Templates']), 
     install_requires=[
 		'xlrd>=1.1.0',
 		'pandas>=0.22.0',
 		'vpython>=7.4.3',
-		#'numbers',
 		'numpy>=1.13.3',
 		'matplotlib>=2.1.0',
+	        'scipy>=0.19.1',
+		'xlsxwriter>=1.0.2',
+	        #'numbers',  # I think all these are native to python so not needed
 		#'tkinter',
 		#'os',
 		#'sys',
-		'scipy>=0.19.1',
-		'xlsxwriter>=1.0.2',
 		],
 
     # List additional groups of dependencies here (e.g. development
@@ -140,21 +71,12 @@ setup(
         # # 'sample': ['package_data.dat'],
     # # },
 
+    #Technically I should have a scripts arguement here and just name the script something useable	
     entry_points={
         'console_scripts': [
             'run_tree = sitchensis.MainBodyScript.py',
         ],
      },
-
-    # List additional URLs that are relevant to your project as a dict.
-    #
-    # This field corresponds to the "Project-URL" metadata fields:
-    # https://packaging.python.org/specifications/core-metadata/#project-url-multiple-use
-    #
-    # Examples listed include a pattern for specifying where the package tracks
-    # issues, where the source is hosted, where to say thanks to the package
-    # maintainers, and where to support the project financially. The key is
-    # what's used to render the link text on PyPI.
     project_urls={  # Optional
         'Bug Reports': 'https://github.com/northcoastmountainrat/sitchensis/issues',
         # 'Funding': 'https://donate.pypi.org',
